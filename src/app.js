4db007e61d9328cd108475e08c7daf566fea077e
DebojyotiRoy15 <debgangulian@gmail.com>
2021-03-11 00:07:04 +0530

    heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast');
 const { response } = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000
 
 const publicDirectory = path.join(__dirname, '../public')
 
@@ -93,6 +94,6 @@ app.get('*', (req, res) => {
     })
 })
 
- app.listen(3000, () => {
+ app.listen(port, () => {
     console.log('Server is running')
 }) 
